a1b67e6ea4e39ff8bcd448f7e127601ebc39c26d
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-07 23:13:14 +0300

    Added a navHeader for the navDrawer in nav_header.xml.

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawer :DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //find the toolbar
+        var toolbar:Toolbar=findViewById(R.id.toolBar)
+        setSupportActionBar(toolbar)
+
+        //find the drawer layout
+        drawer=findViewById(R.id.drawerLyt)
+
+
     }
 }
