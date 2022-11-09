487d3fd098693733fd95906192e4dd848d89008f
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-09 16:10:42 +0300

    Fixed the app crash error by setting CustomTheme to have noActionBar as parent attribute.

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTheme(R.style.CustomTheme)
 
         //find the toolbar
         var toolbar:Toolbar=findViewById(R.id.toolBar)
