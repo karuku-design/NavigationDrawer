d16e9df94a5b47dca1b208a3bb1239dd41b165e0
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-10 15:18:01 +0300

    Added onItemSelect actions on the menu items on MainActivity.kt.

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,NavigationView.OnNavigationItemSelectedListener{
 
     private lateinit var drawer :DrawerLayout
 
@@ -31,6 +34,10 @@ class MainActivity : AppCompatActivity() {
         drawer.addDrawerListener(toogle)
         toogle.syncState()
 
+        //navigation view
+        var nav_view: NavigationView= findViewById(R.id.nav_view)
+        nav_view.setNavigationItemSelectedListener (this)
+
     }
 
     override fun onBackPressed() {
@@ -40,5 +47,20 @@ class MainActivity : AppCompatActivity() {
         }else{
             super.onBackPressed()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.nav_message->getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+            , messageFragment()).commit()
+            R.id.nav_chat->getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                ,chatFragment()).commit()
+            R.id.nav_profile->getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                ,profileFragment()).commit()
+            R.id.nav_share->Toast.makeText(this,"Shared",Toast.LENGTH_LONG).show()
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
